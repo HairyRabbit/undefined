428ecbc01f9326a3196686714fcc7af9fd2f20ca
yuffiy <yfhj1990@hotmail.com>
2016-08-01 16:54:25 +0800

    update elm config.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import $ from 'jquery'
 import { render } from 'react-dom'
 import Snippets from 'Snippets/Main.elm'
 import Slider from 'Slider/index.jsx'
+import SliderPort from 'Slider/port.js'
 
 class App extends Component {
   render() {
@@ -18,28 +19,10 @@ class App extends Component {
 }
 
 function setupPorts(ports) {
-    console.log( ports )
+  console.log( ports )
 
-    ports.getSize.subscribe(uuid => {
-        let $el = $(`[data-uuid=${uuid}]`)
-        //let elWidth = el.getBoundingClientRect().width
-        //let elWidth = el.find('.slider__line').width()
+  SliderPort(ports)
 
-        const getSliderSize = uuid => _ => {
-            let $el = $(`[data-uuid=${uuid}]`).find('.slider__line')
-            return {
-                left:  $el.offset().left,
-                width: $el.width()
-            }
-        }
-
-        let fx = getSliderSize(uuid)
-
-        //console.log($el.find('.slider__line').offset())
-
-        $(window).on('resize', _ => ports.setSize.send(fx()))
-        setTimeout(_ => ports.setSize.send(fx()), 0)
-    })
 }
 
 render(
