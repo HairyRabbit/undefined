d0b8f0b64dc827bbf6c50e89470bc0e556ebbaa7
yuffiy <yfhj1990@hotmail.com>
2016-08-05 19:19:52 +0800

    add el

--- lib/emacs/rabbit/rabbit-web-live/packages.el
+++ lib/emacs/rabbit/rabbit-web-live/packages.el
@@ -2,36 +2,19 @@
 ;;
 ;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
 ;;
-;; Author:  <yufi@DESKTOP-IS3EVI7>
+;; Author:  <Rabbit@yfhj1990@hotmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
 
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `rabbit-web-live-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `rabbit-web-live/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `rabbit-web-live/pre-init-PACKAGE' and/or
-;;   `rabbit-web-live/post-init-PACKAGE' to customize the package as it is loaded.
-
 ;;; Code:
 
 (setq rabbit-web-live-packages
       '(
         stylus-mode
+        jinja2-mode
         editorconfig
         ))
 
@@ -42,15 +25,24 @@
   )
 
 
+
 (defun rabbit-web-live/init-stylus-mode ()
   (use-package stylus-mode
     :defer t
     :init)
   )
 
+(defun rabbit-web-live/init-jinja2-mode ()
+  (use-package jinja2-mode
+    :defer t
+    :init)
+  )
+
 (defun rabbit-web-live/init-editorconfig ()
-  (use-package stylus-mode
-    :defer t)
+  (use-package editorconfig
+    :defer t
+    :init)
+  (editorconfig-apply)
   )
 
 ;;; packages.el ends here
