a230095b8db365ac8aba6dae996a928789631a67
yuffiy <yfhj1990@hotmail.com>
2016-09-03 19:10:19 +0800

    update.

--- lib/emacs/rabbit-tasks.el
+++ lib/emacs/rabbit-tasks.el
@@ -2,6 +2,8 @@
 (require 's)
 (require 'f)
 
+(require 'filenotify)
+
 (defun tasker () ""
   (print 1))
 
@@ -15,3 +17,33 @@ Test path is the absolute path.
 
 
 (absolutePath? "//")
+
+
+(defun on-ntfy (evt)
+  (message "Event %s" evt)
+  )
+
+(file-notify-add-watch "f:/atlantis/.test"
+  '(change attribute-change) 'on-ntfy)
+
+
+(defun rabbit/watcher (pathFn)
+  "\
+Path must be absolute path.
+If file is a directory, changes for all files in that directory will be notified. This does
+not work recursively.
+"
+  (let ((path (funcall pathFn)))
+    (map (lambda (n)
+           (file-notify-add-watch "f:/atlantis/.test"
+             '(change attribute-change) 'on-ntfy)
+           ) path)
+    )
+  )
+
+
+(rabbit/watcher
+  (f-directories "f:/atlantis/.test")
+  )
+
+(f-directories "f:/atlantis/.test")
