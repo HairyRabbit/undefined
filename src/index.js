1a6347489784bcd331177f426b820c2fd01459c3
yuffiy <yfhj1990@hotmail.com>
2016-09-22 19:47:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import Snippets from 'Snippets/Main.elm'
 import Slider from 'Slider/index.jsx'
 import SliderPort from 'Slider/port.js'
 
-//import test from './images/photo.jpg'
-
-console.log(test)
 
 class App extends Component {
   render() {
@@ -26,15 +23,15 @@ function setupPorts(ports) {
   console.log( ports )
 
   SliderPort(ports)
-
 }
 
+/**
 render(
   <App />,
   document.getElementById('main')
 )
 
-/**
+
    ___browserSync___.socket.emit('test', 2333)
 
    var ws = new WebSocket('ws://localhost:9999')
@@ -52,3 +49,174 @@ require.ensure('test2.js', function() {
 	console.log(test)
 })
 */
+
+
+
+
+/*
+   type Date = [1...31]
+   type Month = [1...12]
+ */
+
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+}
+
+function getDateCountInMonth(isLeapYear, mm) {
+  switch(mm + 1) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31
+    case 2:
+      return isLeapYear ? 29 : 28
+    default:
+      return 30
+  }
+}
+
+
+// fx :: Date -> Timestamp
+function computMsAsDay(day) {
+  return 24 * 60 * 60 * 1000 * day
+}
+
+function getDateObjFromTimestamp(timestamp) {
+  return new Date(timestamp)
+}
+
+function getDateFromDateObj(dateObj) {
+  return dateObj.getDate()
+}
+
+function getYearFromDateObj(dateObj) {
+  return dateObj.getFullYear()
+}
+
+function getMonthFromDateObj(dateObj) {
+  return dateObj.getMonth()
+}
+
+function getWeekFromDateObj(dateObj) {
+  return dateObj.getDay()
+}
+
+function divideInt(a, b) {
+  return parseInt(a / b)
+}
+
+
+/*
+function sameWeekDates(a, b) {
+  b - divideInt(b / 7) * 7
+}
+*/
+
+
+
+function normalizeWeek(week) {
+  return week === 0 ? 6 : week - 1
+}
+
+
+
+// fx :: Timestamp -> Date -> Timestamp
+function computFirstDateInMonth(target, curr) {
+  return target - computMsAsDay(curr - 1)
+}
+function computLastDateInMonth(target, sum, curr) {
+  return target + computMsAsDay(sum - curr)
+}
+
+function prepend() {
+
+}
+
+function constCurrentMonthDates(count) {
+  return new Array(count)
+}
+
+function constArray(len) {
+  let out = []
+  for(let i = 0; i < len; i++) out.push(i)
+  return out
+}
+
+function penpendArray(array, len) {
+  let out = []
+  for(let i = 0, j = 0; i < len; i++, j--) out.push(j - 1)
+  return out.reverse().concat(array)
+}
+
+function appendArray(array, start, len) {
+  let out = []
+  for(let i = 0, j = start + 1; i < len; i++, j++) out.push(j)
+  return array.concat(out)
+}
+
+
+function renderHeader() {
+  return ['日', '一', '二', '三', '四', '五', '六',].join('  ')
+}
+
+function formatData(matcher, date) {
+  let str = date.toISOString()
+  let dateStr = str.substr(0, str.indexOf('T')).split('-')
+  if(matcher === 'dd') return dateStr[2]
+  return dateStr.join('-')
+}
+
+
+function renderBody(array) {
+  let len = array.length
+  let out = Array(len)
+  for(let i = 0; i < len; i++) {
+    let cur = array[i]
+    let str = formatData('dd', cur)
+    out[i] = (i + 1) % 7 !== 0 ? str + '  ' : str + '\n'
+  }
+  return out.join('')
+}
+
+function renderView(array) {
+  let header = renderHeader()
+  let body   = renderBody(array)
+  return [header, body].join('\n')
+}
+
+function main(start) {
+  let dateObj = getDateObjFromTimestamp(start)
+  let dd = getDateFromDateObj(dateObj)
+  let firstDateInMonth = getDateObjFromTimestamp(computFirstDateInMonth(start, dd))
+  let yy = getYearFromDateObj(dateObj)
+  let mm = getMonthFromDateObj(dateObj)
+  let dateCount = getDateCountInMonth(isLeapYear(yy), mm)
+  let lastDateInMonth = getDateObjFromTimestamp(computLastDateInMonth(start, dateCount, dd))
+  let firstWeek = normalizeWeek(getWeekFromDateObj(firstDateInMonth)) + 1
+
+  let currArray = constArray(dateCount)
+  let prevArray = penpendArray(constArray(dateCount), firstWeek)
+  let nextArray = appendArray(prevArray, prevArray[prevArray.length - 1], 6 * 7 - dateCount - firstWeek)
+
+  let calendar = nextArray.map(n => getDateObjFromTimestamp(computFirstDateInMonth(start, dd) + computMsAsDay(n)))
+
+
+
+  console.log(
+    //nextArray,
+    //nextArray.map(n => getDateObjFromTimestamp(computFirstDateInMonth(start, dd) + computMsAsDay(n)).getDate()),
+    //firstDateInMonth,
+    //nextArray.map(n => computMsAsDay(n)),
+    //getDateObjFromTimestamp(firstDateInMonth - computMsAsDay(firstWeek))
+    renderView(calendar)
+  )
+}
+
+main(1475205920779) //Date.now()
+
+
+import 'runtime/function/partial'
